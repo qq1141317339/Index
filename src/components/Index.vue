fb5457fb862d8588c8ccdf28a915b4fc54ab9d83
LUO <LUO@DESKTOP-DIU94DM>
2019-12-29 22:50:53 +0800

    测试发布

---
+++ src/components/Index.vue
@@ -0,0 +1,80 @@
+<template>
+<div>
+<dx-data-grid 
+:visible="show"
+:data-source="dataSource"
+:show-borders="true"
+allow-column-resizing="true"
+:remote-operations="true"
+@option-changed="clickShow"
+>
+    <DxColumn
+      data-field="CompanyName"
+      :width.sync="width[0]"
+    />
+    <DxColumn
+      data-field="City"
+      :width.sync="width[1]"
+    />
+    <DxColumn
+      data-field="State"
+      :width.sync="width[2]"
+    />
+    <DxColumn
+      data-field="Phone"
+      :width.sync="width[3]"
+    />
+    <DxColumn
+      data-field="Fax"
+      :width.sync="width[4]"
+    />
+</dx-data-grid>
+<dx-button
+text="删除一行"
+@click="deleteRow"
+/>
+<dx-button
+text="查看宽度"
+@click="deleteColumn"
+/>
+<dx-button
+text="显示grid"
+@click="clickShow"
+/>
+</div>
+</template>
+
+<script>
+import { DxDataGrid, DxColumn} from "devextreme-vue/data-grid";
+import {customers} from '../data/data.js';
+import DxButton from 'devextreme-vue/button';
+import notify from 'devextreme/ui/notify';
+export default {
+    name:'Index',
+    components:{
+    DxDataGrid,
+    DxColumn,
+    DxButton
+    },
+    data(){
+        return{
+            width:["20%","20%","20%","20%","20%"],
+            show:true,
+            dataSource:customers,
+            columns:["CompanyName","City","State","Phone","Fax"]
+
+        }
+    },
+    methods:{
+        deleteRow(){
+            this.dataSource.splice(0,1);
+        },
+        deleteColumn(){
+            notify(this.width.join(","))
+        },
+        clickShow(){
+            this.show=!this.show;
+        }
+    }
+}
+</script>
